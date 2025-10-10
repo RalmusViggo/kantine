cd7a0307a9d360320e4d33de47989adcf8b67cde
“RalmusViggo” <rasmus.lang_holmen@icloud.com>
2025-10-10 13:20:07 +0200

    jeg lagde alle de andre html filene jeg skal ha, en navigasjonsbar, og la til rutene til html filene inn i app.py

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/kontakt.html')
+def kontakt():
+    return render_template("kontakt.html")
+
+@app.route('/meny.html')
+def meny():
+    return render_template("meny.html")
+
+@app.route('/varer.html')
+def varer():
+    return render_template("varer.html")
+
 if __name__ == "__main__":
     app.run()
